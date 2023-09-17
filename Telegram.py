4b5b6de085dd5d9e526071833fb1a6ec31d71ade
NSarkisov <99996135+NSarkisov@users.noreply.github.com>
2023-09-17 20:52:44 +0300

    тг

--- Telegram.py
+++ Telegram.py
@@ -26,25 +26,37 @@ con = sl.connect(database)
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text == '/start':
-        user_id = message.from_user.id
-        name = message.from_user.first_name
 
         bot.send_message(message.chat.id, f"Привет {message.from_user.first_name}!\nМы рады приветствовать вас")
         bot.send_message(message.chat.id,
                          'Выберите интересующий для вас раздел')
-        con.execute("INSERT OR IGNORE INTO Пользователи (Имя, Аватарка, ID TG) values (?, ?, ?)", )
-        # user = message.from_user
-        # photos = bot.get_user_profile_photos(user.id)
-        # if photos.total_count > 0:
-        #     photo = photos.photos[0][-1]
-        #     file_id = photo.file_id
-        #     file_info = bot.get_file(file_id)
-        #     file_path = file_info.file_path
-        #     with open('avatar.jpg', 'wb') as file:
-        #         file.write(bot.download_file(file_path))
-        #     with open("avatar.jpg", 'rb') as photo:
-        #         bot.send_photo(message.chat.id, photo)
-        #     os.remove("avatar.jpg")
+
+        name = message.from_user.first_name
+        user_id = message.from_user.id
+
+        with con:
+            # con.execute("INSERT OR IGNORE INTO Пользователи (Имя, ID TG) values (?, ?)", [name, user_id])
+            data1 = con.execute("Select * from MenuSections")
+            print(data1.fetchall())
+
+        photos = bot.get_user_profile_photos(user_id)
+        if photos.total_count > 0:
+            photo = photos.photos[0][-1]
+            file_id = photo.file_id
+            file_info = bot.get_file(file_id)
+            file_path = file_info.file_path
+            with open('avatar.jpg', 'wb') as file:
+                file.write(bot.download_file(file_path))
+            with open("avatar.jpg", 'rb') as file:
+                image_data = file.read()
+                with con:
+                    con.execute(f"INSERT OR IGNORE INTO Пользователи (Аватарка) values (?)"
+                                f" where Имя = {name} and ID TG = {user_id}", image_data)
+            os.remove("avatar.jpg")
+
+
+
+
 
 
 @bot.callback_query_handler(func=lambda call: True)
